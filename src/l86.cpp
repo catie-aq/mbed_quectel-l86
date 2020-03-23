b3e884a3098a929ed5262572717c4d706d8b0c3c
n_gibaud_catie <n.gibaud@catie.fr>
2020-03-23 11:50:37 +0100

    Remove SWO library usage

--- src/l86.cpp
+++ src/l86.cpp
@@ -25,7 +25,6 @@ void L86::stop_attach()
 
 void L86::write_pmtk_message(Pmtk_message message){
 	/* Formation de le trame PMTK */
-	SWO swo;
 	char packet[100];
 	char packet_temp[100];
 	sprintf(packet, "$PMTK%c%c%c", message.packet_type[0], message.packet_type[1], message.packet_type[2]);
@@ -42,8 +41,6 @@ void L86::write_pmtk_message(Pmtk_message message){
 
 	sprintf(packet, "%s%X\r\n", packet_temp, checksum);
 
-	swo.printf("L86::write_pmtk_message : %s\n", packet);
-
 	/* Send packet until received ack and get confirmation that command succeeds */
 	do {
 		_uart->write((uint8_t *)packet, strlen(packet), NULL);
@@ -59,12 +56,10 @@ void L86::write_pmtk_message(Pmtk_message message){
 					index++;
 				}
 			}
-			swo.printf("L86::write_pmtk_message : Message : %s\n", ack);
 			if (ack[5] == '0' && ack[6] == '0' && ack[7] == '1'){	/* ack trame */
 				_waiting_ack = false;
 				if (ack[9] == message.packet_type[0] && ack[10] == message.packet_type[1] && ack[11] == message.packet_type[2]){
 					if (ack[13] == '3'){
-						swo.printf("L86::write_pmtk_message : Ack received\n");
 						_pmtk_command_result = true;
 					}
 				}
@@ -192,7 +187,6 @@ void L86::set_navigation_mode(NavigationMode navigation_mode){
 }
 
 void L86::set_position_fix_interval(uint16_t interval){
-	SWO swo;
 	Pmtk_message message;
 	message.packet_type[0] = '2';
 	message.packet_type[1] = '2';
@@ -219,8 +213,6 @@ void L86::set_position_fix_interval(uint16_t interval){
 		message.parameters[0][i] = s_interval[i];
 	}
 	message.parameters[0][size] = '\0';
-	swo.printf("L86::set_position_fix_interval : Parametre : %s\n", message.parameters[0]);
-
 	message.ack = true;
 	write_pmtk_message(message);
 }
